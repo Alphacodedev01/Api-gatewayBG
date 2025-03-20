b1b9c015d67e8dfe4ed0bffe4ff051461ac15d6e
SebastianQuesadaRios <sebastianquesadarios@gmail.com>
2025-03-20 13:56:49 -0500

    Correcion al error 500

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,16 @@ const proxy = require('@fastify/http-proxy');
 
 module.exports = async function (fastify, opts) {
   fastify.register(proxy, {
-    upstream: process.env.AUTH_SERVICE_URL,
+    upstream: 'https://auth-service-bg.vercel.app',
     prefix: '/api/auth',
-    rewritePrefix: '/auth'
+    rewritePrefix: '/auth',
+    replyOptions: {
+      rewriteRequestHeaders: (originalReq, headers) => {
+        return {
+          ...headers,
+          'host': 'auth-service-bg.vercel.app'
+        };
+      }
+    }
   });
 };
